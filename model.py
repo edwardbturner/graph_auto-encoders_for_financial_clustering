98c7341506fc0fc6b6368b63a782433e8b697130
Edward Turner <edward.turner@mansfield.ox.ac.uk>
2021-10-24 20:19:59 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,31 @@
+import torch
+from torch_geometric.nn import GAE
+from torch_geometric.nn import GCNConv
+
+
+class GCNEncoder(torch.nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels):
+        super(GCNEncoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, mid_channels, cached=True)
+        self.conv2 = GCNConv(mid_channels, 2 * out_channels, cached=True)
+        self.conv3 = GCNConv(2 * out_channels, out_channels, cached=True)
+        
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        return self.conv3(x, edge_index)
+    
+
+def model(data, mid_channels, out_channels, L2_rate, learning_rate):
+    model = GAE(GCNEncoder(data.num_features, mid_channels, out_channels))
+    # This by default sets the decoder to be the inner product
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    x = data.x.to(device)
+    train_pos_edge_index = data.train_pos_edge_index_f.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=L2_rate)
+    
+    
+    return(model, x)
+
